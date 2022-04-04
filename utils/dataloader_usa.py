026f68b38fb4cabed9eb17fb15f850571868e367
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2022-04-05 00:31:37 +0200

    Evaluate the model on BDD trajectories sample

--- utils/dataloader_usa.py
+++ utils/dataloader_usa.py
@@ -72,12 +72,51 @@ class TrainDataloader(Dataset):
 
 class TestDataloader(Dataset):
     def __init__(self, args):
-        print(args.polar)
-        self.polar = args.polar
+        """Return dictionary of aerials path key and tuple of ground dir path, number of taken aerials and number of taken grounds"""
+        grds_root_path = "/kaggle/input"
+        aerials_root_path = '/kaggle/input/polar-transformation-0-5000/polar_aerial_images/'
 
-        self.img_root = args.dataset_dir
-        self.img_root_polar = self.img_root.replace('/cvusa-dataset/cvusa-localization', '/dsm-dynamic-similarity-matching/Data/CVUSA')
-        self.test_list = self.img_root + 'splits/val-19zl.csv'
+        aerial_dirs = os.listdir(aerials_root_path)
+        grd_parts = [f'frame-extraction-{i}-{i+1}' for i in range(0, 19, 2)]
+        aerial_files_path = []
+        ground_files_path = []
+
+        for grd_part in grd_parts:
+            part_path = f'{grds_root_path}/{grd_part}/frames'
+            for simple_dir in os.listdir(part_path): 
+
+                if simple_dir in aerial_dirs:
+                    aerial_path = os.path.join(aerials_root_path, simple_dir)
+                    grd_path = os.path.join(part_path, simple_dir)
+                    aerial_dir = sorted(os.listdir(aerial_path))
+                    ground_dir = sorted(os.listdir(grd_path))
+                    num_ground = len(ground_dir)
+                    num_aerial = len(aerial_dir)
+
+                    for i in range(num_aerial):
+                        aerial_file_path = os.path.join(aerial_path, aerial_dir[i])
+                        grd_file_path = [os.path.join(grd_path, ground_dir[j]) for j in range(i*5, min(i*5+5, num_ground))]
+                        aerial_files_path.append(aerial_file_path)
+                        ground_files_path.append(grd_file_path)
+
+        i = 0
+        while i < len(ground_files_path):
+            if len(ground_files_path[i]) < 1:
+                del ground_files_path[i]
+                del aerial_files_path[i]
+            else:
+                i +=1
+
+
+        ground_files_path = [path[0] for path in ground_files_path] # Retrieve the first ground image and drop the remaining
+
+        SELECTED_INDICES_1 =  [210, 211, 212, 290, 305, 319, 330, 355, 400, 505, 740, 800, 840, 900, 870, 935, 960, 965, 967, 990, 1006, 1020, 1095, 1135, 1200, 1204, 1218, 1229, 1297, 1305, 1311, 1355, 1380, 1382, 1497, 1500, 1585, 1595, 1600, 1900, 1960, 1980, 1995, 2020, 2050, 2210, 2220, 2225, 2280, 2400, 2395, 2437, 2545, 2705, 3010, 3025, 3080, 3110, 3235, 3505, 3870, 4400, 4410, 5500, 6010]
+        SELECTED_INDICES_2 =  [7536, 7566, 7596, 7616, 7621, 7626, 7641, 7651, 7661, 7676, 7691, 7706, 7711, 7726, 7741, 7751, 7766, 7796, 7816, 7836, 7846, 7906, 7956, 7996, 8156, 8166, 8231, 8321, 8351, 8481, 8596, 8626, 8646, 8691, 8761, 8781, 8836, 8876, 8896, 8916, 8951, 9011, 9121, 9201, 9336, 9346, 9381, 9571, 9586, 9596, 9601, 9666, 9736, 10166, 12461]
+        SELECTED_INDICES = SELECTED_INDICES_1 + SELECTED_INDICES_2
+        SELECTED_AERIAL_POLAR = [aerial_files_path[index] for index in SELECTED_INDICES]
+        SELECTED_GROUND = [ground_files_path[index] for index in SELECTED_INDICES]
+
+        self.polar = args.polar
 
         self.transform = transforms.Compose(
             [transforms.Resize((args.img_size[0], args.img_size[1])),
@@ -90,36 +129,23 @@ class TestDataloader(Dataset):
             transforms.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225))] )
 
         self.__cur_test_id = 0  # for training
-        self.id_test_list = []
-        self.id_test_idx_list = []
-        with open(self.test_list, 'r') as file:
-            idx = 0
-            for line in file:
-                data = line.split(',')
-                pano_id = (data[0].split('/')[-1]).split('.')[0]
-                # satellite filename, streetview filename, pano_id
-                if self.polar:
-                    item1 = self.img_root_polar + data[0].replace('bing', 'polar').replace('jpg', 'png')
-                else:
-                    item1 = self.img_root + data[0]
-                
-                item2 = self.img_root + data[1]
+        self.id_test_list = (SELECTED_AERIAL_POLAR, SELECTED_GROUND)
+        self.id_test_idx_list = SELECTED_INDICES
 
-                self.id_test_list.append([item1, item2, pano_id])
-                
-                self.id_test_idx_list.append(idx)
-                idx += 1
-        self.test_data_size = len(self.id_test_list)
-        print('InputData::__init__: load', self.test_list, ' data_size =', self.test_data_size)
+        self.test_data_size = len(self.id_test_list[0])
+        print(f'Number of polar aerial images {len(self.id_test_list[0])}')
+        print(f'Number of ground images {len(self.id_test_list[1])}')
+        print('polar', args.polar)
+
 
 
     def __getitem__(self, idx):
         
-        x = Image.open(self.id_test_list[idx][1]).convert('RGB')
+        x = Image.open(self.id_test_list[1][idx]).convert('RGB')
         
         x = self.transform(x)
 
-        y = Image.open(self.id_test_list[idx][0]).convert('RGB')
+        y = Image.open(self.id_test_list[0][idx]).convert('RGB')
 
         if self.polar:
             y = self.transform(y)
